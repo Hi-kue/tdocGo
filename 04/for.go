e87599ea52d5bcd9125abe5a8aa93a5f3bf69baa
Muhammad Bilal Khan <lofi.audit@gmail.com>
2023-08-29 14:25:51 -0400

    Updated 04/for.go

--- 04/for.go
+++ 04/for.go
@@ -2,8 +2,43 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
-	fmt.Print("for.go\n")
+	arr := []int{
+		1, 2, 3, 4, 5, 6,
+	}
+
+	i := 1
+	for i <= 4 {
+		fmt.Println(i)
+		i++
+	}
+
+	for j := 1; j <= 10; j++ {
+		random := rand.Intn(j)
+		arr = append(arr, random)
+	}
+
+	fmt.Println(arr)
+
+
+	// for loop to only get odd numbers from the array
+	for _, value := range arr {
+		if value % 2 == 0 {
+			continue
+		}
+		fmt.Println(value)
+	}
+
+	// this is another variation to the above for loop
+	for i := 0; i < len(arr); i++ {
+		// check whether the number is even
+		if arr[i] % 2 == 0 {
+			continue
+		}
+		// print only odd numbers
+		fmt.Println(arr[i])
+	}
 }
