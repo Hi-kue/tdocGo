9f789b1494df45db6489c0d12320f4a86c816c31
Muhammad Bilal Khan <lofi.audit@gmail.com>
2023-08-29 15:48:00 -0400

    Created 05 directory, ifelse.go

---
+++ 05/ifelse.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+	If/Else Statements in Go
+	
+	Notes:
+		- If/else statements are conditional statements, like any other language
+		- If/else statements can be used in conjunction with for loops, as well as switch statements
+		- If/else statements can also be used to check for errors, which is a common use case
+*/
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("..")
+}
